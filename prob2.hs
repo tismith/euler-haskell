ab0a0fb4e3fd38a8b227696f6b3e00f0610fff6a
Toby Smith <toby.smith@leica-geosystems.com>
2010-12-01 12:11:16 +1000

    Minor refactor

--- prob2.hs
+++ prob2.hs
@@ -1,6 +1,6 @@
 
 myFib a b = a:(myFib b (a+b))
-resultList = takeWhile (\x -> x < 4000000) (filter (even) (myFib 1 2))
+resultList = takeWhile (< 4000000) $ filter (even) (myFib 1 2)
 main = do
         let result = sum resultList
         putStrLn (show result)
