51b349020ef1305cf27cc96d0339c860ed9d8359
Toby Smith <toby@tismith.id.au>
2010-12-01 21:51:24 +1000

    WIP

--- prob3.hs
+++ prob3.hs
@@ -1,5 +1,15 @@
-candidates x = [y | y <- [(x-1), (x-2)..2], x `mod` y == 0]
-isPrime x = all (\y -> mod x y /= 0) [2..(x-1)]
-primeFactors = filter (isPrime) . candidates
-greatestPrimeFactor = head $ primeFactors 600851475143
-main = do putStrLn (show greatestPrimeFactor)
+
+candidates x = [y | y <- [2..(x-1)], x `mod` y == 0]
+{--
+candidates x = let sqrtX = truncate $ sqrt $ fromIntegral x
+                in 
+            [y | y <- [2..sqrtX], x `mod` y == 0]
+--}
+--isPrime x | even x = False
+--          | otherwise = all (\y -> mod x y /= 0) [3,5..(truncate $ sqrt $ fromIntegral x)]
+--primeFactors = filter (isPrime) . candidates
+cull [] = []
+cull (x:xs) = x : (cull (filter (\y -> mod y x /= 0) xs))
+primeFactors = cull . candidates
+greatestPrimeFactor = last . primeFactors 
+main = do putStrLn (show $ greatestPrimeFactor 600851475143)
