e336a8e0eaeac7903dff8b214a370abdecca7120
Toby Smith <toby@tismith.id.au>
2010-12-10 07:22:32 +1000

    Much faster version of my prime number generator

--- prob7.hs
+++ prob7.hs
@@ -4,8 +4,16 @@ main = do
     args <- getArgs
     putStrLn $ show $ primes !! (pred $  read $ head args)
 
-primes = primes' 2 []
+primes = 2:(primes' 3 [2])
+
 primes' x xs 
-    | any (\a -> x `mod` a == 0) xs = primes' (succ x) xs
-    | otherwise = x:(primes' (succ x) (x:xs))
+    | check x xs = x:(primes' (x+2) (xs ++ [x]))
+    | otherwise = primes' (x+2) xs
+
+check p [] = True
+check p (x:xs)
+    | x > (ceiling $ sqrt $ fromIntegral p) = True
+    | p `mod` x == 0 = False
+    | otherwise = check p xs
+
 
